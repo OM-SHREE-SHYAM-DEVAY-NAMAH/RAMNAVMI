632f9bade8c584151f9a3cc514822fa5d38ec97d
SACHIN <119405675+sachin-fullstack@users.noreply.github.com>
2023-03-29 22:13:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+/src
+|--/components
+|  |--Header.js
+|  |--About.js
+|  |--Skills.js
+|  |--Portfolio.js
+|  |--Contact.js
+|--/images
+|  |--profile.jpg
+|--/App.js
+|--/index.js
+|--/index.css
+|--/index.html
